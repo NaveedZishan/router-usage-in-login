bce49102ca431090f662dfc4403d7b15d591d50d
Naveed Zishan <navidzeeshan1988@.com>
2023-02-01 13:17:44 +0530

    first commit

---
+++ router1/src/AboutUs.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+class AboutUs extends React.Component{
+
+
+render(){
+
+    return(
+
+
+        <h1> ABOUT US</h1>
+    )
+}
+
+}
+export default AboutUs;
+
---
+++ router1/src/Gallery.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+class Gallery extends React.Component{
+
+
+render(){
+
+    return(
+
+
+        <h1> GALLERY </h1>
+    )
+}
+
+}
+export default Gallery;
+
